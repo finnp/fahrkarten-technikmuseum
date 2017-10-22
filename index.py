9797f3d6bf04d8f229162b14175f0e3e94b4283b
Finn Pauls <derfinn@gmail.com>
2017-10-22 18:32:30 +0200

    improvement with two different ways of detecting the tickets

--- index.py
+++ index.py
@@ -2,18 +2,20 @@ import cv2, numpy as np
 import sys
 import imutils
 
-# finding contours
-def contorize (img, a):
-    _, contours, _ = cv2.findContours(a.copy(), cv2.RETR_LIST,
-                               cv2.CHAIN_APPROX_SIMPLE)
+retr_type = cv2.RETR_EXTERNAL # cv2.RETR_LIST
+contour_algorithm = cv2.CHAIN_APPROX_TC89_KCOS # cv2.CHAIN_APPROX_SIMPLE
 
-    contours = filter(lambda cont: cv2.contourArea(cont) > 400000, contours)
+
+# finding contours
+def contorize (img, a, color):
+    _, contours, _ = cv2.findContours(a.copy(), retr_type, contour_algorithm)
+    contours = filter(lambda cont: cv2.contourArea(cont) > 400000, contours) # 20.000 to get more infor
 
     rects = []
     for cont in contours:
         rect = cv2.approxPolyDP(cont, 40, True).copy().reshape(-1, 2)
         rect = cv2.convexHull(rect)
-        if (len(rect) > 15): continue
+        if (len(rect) > 15): continue # possibly not needed when comparing the areas
         area = cv2.contourArea(rect)
         x,y,width,height = cv2.boundingRect(rect)
         rect_area = width * height
@@ -21,7 +23,7 @@ def contorize (img, a):
         if (area_diff > 60000): continue
         rects.append(rect)
 
-    cv2.drawContours(img, rects,-1,(0,255,0),10)
+    cv2.drawContours(img, rects,-1,color,10)
 
 
 
@@ -42,10 +44,14 @@ dilation = cv2.dilate(binary,smallkernel,iterations = 1)
 kernel = np.ones((11,11),np.uint8)
 erosion = cv2.erode(dilation,kernel,iterations = 1)
 
-# HoughLines?
 
-# is this the right tool?
-# edges = cv2.Canny(binary, 0, 100) # this produces the wrong output
-contorize(original, erosion)
+rough_kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(40,40))
+rough_dilation = cv2.dilate(binary, rough_kernel)
+rough_erosion = cv2.erode(rough_dilation,kernel,iterations = 1)
+# blur again here?
+edges = cv2.Canny(rough_erosion, 0, 85, apertureSize=3)
+
+contorize(original, erosion, (0,255,0))
+contorize(original, edges, (0,0,255))
 
 cv2.imwrite('output/test.png', original)
