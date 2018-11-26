f1e01ccc32b85ee2c57284a84984173c7d97604c
Finn Pauls <derfinn@gmail.com>
2018-11-26 21:37:33 +0100

    Post a picture

--- index.py
+++ index.py
@@ -1,14 +1,21 @@
 from sanic import Sanic
-from sanic.response import json
+from sanic.response import json, text
 from find_tickets import find_tickets
 import cv2
+import numpy as np
 
 app = Sanic()
 
 @app.route("/")
 async def test(request):
-    example = cv2.imread('test.jpg')
-    tickets = find_tickets(example)
+    return text("Usage: curl -X POST <server>/findtickets -F 'data=@test.jpg'. Returns a list of found tickets")
+
+@app.route("findtickets", methods=['POST'])
+async def ticket(request):
+    img_str = request.files['data'][0].body
+    arr = np.frombuffer(img_str, np.uint8)
+    img = cv2.imdecode(arr, cv2.IMREAD_COLOR)
+    tickets = find_tickets(img)
     return json(tickets)
 
 if __name__ == "__main__":
